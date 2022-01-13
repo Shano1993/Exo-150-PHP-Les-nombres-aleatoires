07780fb56ee1efb2d2dde9a59ef012157daa91e2
Shano1993 <stefan.hanotiau@gmail.com>
2022-01-13 10:23:51 +0100

    php

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
  */
 
 // TODO Votre code ici.
-
+$nmbr1 = rand(0, 50);
+echo "<div>Mon nombre aléatoire est $nmbr1</div>";
 
 
 /**
@@ -13,7 +14,8 @@
  */
 
 // TODO Votre code ici.
-
+$nmbr2 = rand(0, getrandmax());
+echo "<p>Mon nombre aléatoire est $nmbr2</p>";
 
 /**
  * 3. Générez un nombre aléatoire compris entre 0 et 50
@@ -22,6 +24,13 @@
  */
 
 // TODO Votre code ici.
+$nmbr3 = rand(0, 50);
+if ($nmbr3 <= 25) {
+    echo "<p>Votre nombre est le $nmbr3, il est inférieur à 25, Vous avez gagnez !</p>";
+}
+else {
+    echo "<p>Votre nombre est le $nmbr3, il est supérieur à 25, Vous avez perdu !</p>";
+}
 
 
 /**
@@ -31,10 +40,17 @@
  */
 
 // TODO Votre code ici.
+function nmbrAleatory($min, $max) {
+    $nmbrRandom = rand($min, $max);
+    if ($max > $nmbrRandom && $nmbrRandom > ($max -100)) {
+        nmbrAleatory($min, $max);
+    }
+    else {
+        echo $nmbrRandom;
+    }
+}
 
-
-
-
+nmbrAleatory(0, 1000);
 
 
 
